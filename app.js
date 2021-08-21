6ec16c935d14ad670b3bce174e5df48d7b022aef
Mayolk <semir_begagic@yahoo.com>
2021-08-21 14:37:44 +0200

    Make functionality to place a sign on the board

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 const GameBoard = (function() {
 
-  const gameboardArray = ['x', 'x', 'o', 'o', '', 'x', 'x', 'x', 'o'];
+  const gameboardArray = ['', '', '', '', '', '', '', '', ''];
   const gameboardUI = document.querySelectorAll('.board-field'); 
+
+  
+  const placeSign = function() {
+
+    let pickedField = this.getAttribute('data-board-index');
+
+    if (gameboardArray[pickedField] === '') {
+      
+      gameboardArray[pickedField] = 'x';
+      fillBoard();
+
+    }
+
+  }
 
   const fillBoard = function() {
 
     for (let i = 0; i < gameboardArray.length; i++) {
       gameboardUI[i].textContent = gameboardArray[i];
     }
+
   };
 
+  gameboardUI.forEach( field => { field.addEventListener('click', placeSign) });
+
   return {
-    fillBoard
-  }
+
+  };
+
 }());
 
-GameBoard.fillBoard();
+
+
+
+// GameEngine
+
+
+// PlayerFactory
