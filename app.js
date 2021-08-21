ca92341859ef01908bc4a806922942ba564bd347
Mayolk <semir_begagic@yahoo.com>
2021-08-21 14:02:50 +0200

    Make functionality to till board

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const GameBoard = (function() {
 
-  const gameboardArray = [x, x, o, o, o, x, x, x, o];
-
+  const gameboardArray = ['x', 'x', 'o', 'o', '', 'x', 'x', 'x', 'o'];
   const gameboardUI = document.querySelectorAll('.board-field'); 
 
   const fillBoard = function() {
 
+    for (let i = 0; i < gameboardArray.length; i++) {
+      gameboardUI[i].textContent = gameboardArray[i];
+    }
   };
 
   return {
-
+    fillBoard
   }
 }());
+
+GameBoard.fillBoard();
