1cfaca60ea95886760da69580f2b57f868f25ead
Mayolk <semir_begagic@yahoo.com>
2021-08-21 13:02:28 +0200

    Make HTML board

---
+++ app.js
@@ -0,0 +1,14 @@
+const GameBoard = (function() {
+
+  const gameboardArray = [x, x, o, o, o, x, x, x, o];
+
+  const gameboardUI = document.querySelectorAll('.board-field'); 
+
+  const fillBoard = function() {
+
+  };
+
+  return {
+
+  }
+}());
